127ff2b2ceb31a86bc83fc0a4f1d969b0f0cdabc
Agustín Prieto <agustinps@users.noreply.github.com>
2022-06-22 18:59:25 +0200

    Update RabbitMQProducer.cs

--- RabbitMQ.API/Producers/RabbitMQProducer.cs
+++ RabbitMQ.API/Producers/RabbitMQProducer.cs
@@ -7,6 +7,8 @@ namespace RabbitMQ.API.Producers
 {
     public class RabbitMQProducer : IMessageProducer
     {
+        //Productor de mensaje para RabbitMQ que implementa el método SendMessage de la interfaz IMessageProducer
+        
         //private readonly IConfiguration configuration;
 
         //Si queremos obtener la configuración de appsetting para crear el ConnectionFactory
@@ -17,14 +19,26 @@ namespace RabbitMQ.API.Producers
 
         public void SendMessage<T>(T message, string routingKey)
         {            
+            //Creamos la facotria de conexiones en el servidor.
+            //Lo ideal sería utilizar IConfiguration para obtener estos datos pero por simplificar 
+            //utilizamos localhost de forma estática. No se utiliza usuario y contraseña por lo que 
+            //se asignarán los datos por defecto guest - guest.
             var factory = new ConnectionFactory() { HostName = "localhost" };
+            //Creamos la conexión.
             var connection = factory.CreateConnection();
+            //Creamos el canal de comunicación
             using(var channel = connection.CreateModel())
             {
+                //Declaramos la cola en la publicar el mensaje por el canal creado
                 channel.QueueDeclare(routingKey, exclusive: false);
+                //Serializamos el mensaje y lo convertimos en byte[] para poder enviarlo
                 var jsonObject = JsonSerializer.Serialize(message);
                 var body = Encoding.UTF8.GetBytes(jsonObject);
+                //Publicamos el mensaje en el agente de enrutamiento (exchange) que será el que 
+                //envíe el mensaje a la cola correspondiente indicada en routingKey.                 
                 channel.BasicPublish(exchange: "", routingKey: routingKey, body: body);
+                //Allí quedará a la espera de que un suscriptor consuma este mensaje.
+                //Esto lo veremos desde el proyecto de aplicación de consola.
             }
         }
     }
